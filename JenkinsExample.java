342429aa6a763e8e70430afd19e3848f066d6e21
melodysujitha <138870319+melodysujitha@users.noreply.github.com>
2023-10-01 09:06:14 +0530

    Initial Commit

---
+++ JenkinsExample.java
@@ -0,0 +1,11 @@
+public class JenkinsExample {
+
+	public static void main(String[] args) {
+		System.out.println("Java");
+        
+		System.out.println("This is my first java project");
+        
+        System.out.println("Java using for Selenium automation");
+	}
+
+}
