aedee137630561706ebe36f9d718b0498c0d3b19
melodysujitha <138870319+melodysujitha@users.noreply.github.com>
2023-10-01 10:46:56 +0530

    NGROK change

--- JenkinsExample.java
+++ JenkinsExample.java
@@ -1,7 +1,7 @@
 public class JenkinsExample {
 
 	public static void main(String[] args) {
-		System.out.println("Java");
+		System.out.println("NGROK");
         
 		System.out.println("Jenkis");
         
