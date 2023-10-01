61631360db18c57ab776c9ca62c931328a008b21
melodysujitha <138870319+melodysujitha@users.noreply.github.com>
2023-10-01 10:47:54 +0530

    update

--- JenkinsExample.java
+++ JenkinsExample.java
@@ -3,7 +3,7 @@ public class JenkinsExample {
 	public static void main(String[] args) {
 		System.out.println("NGROK");
         
-		System.out.println("Jenkis");
+		System.out.println("Jenkins");
         
         System.out.println("Git");
 	}
