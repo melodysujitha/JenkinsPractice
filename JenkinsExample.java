0e752d61606f83165d432ce82d5cf13249bfb305
melodysujitha <138870319+melodysujitha@users.noreply.github.com>
2023-10-01 10:35:26 +0530

    second commit

--- JenkinsExample.java
+++ JenkinsExample.java
@@ -3,7 +3,7 @@ public class JenkinsExample {
 	public static void main(String[] args) {
 		System.out.println("Java");
         
-		System.out.println("This is my first java project");
+		System.out.println("Jenkis");
         
         System.out.println("Java using for Selenium automation");
 	}
